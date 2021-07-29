1c893bb613befb1fbb732c00d5e1717057f9cd37
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-29 15:36:20 +0530

    SaveCourse Test Case

--- src/test/java/com/springjpa/Repo/CourseMatrialRepoTest.java
+++ src/test/java/com/springjpa/Repo/CourseMatrialRepoTest.java
@@ -1,5 +1,9 @@
 package com.springjpa.Repo;
 
+import com.springjpa.Entity.CourceMaterial;
+import com.springjpa.Entity.Courses;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,4 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CourseMatrialRepoTest {
 
+    @Autowired
+    CourseMatrialRepo courseMatrialRepo;
+    @Test
+    public  void SaveCourse(){
+        Courses courses=Courses.builder()
+                .title("C++")
+                .credits(6)
+                .build();
+        CourceMaterial courceMaterial= CourceMaterial.builder()
+                .url("www.google.com")
+                .course(courses)
+                .build();
+        courseMatrialRepo.save(courceMaterial);
+    }
 }
