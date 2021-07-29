b9cc0adb9f6f4f12d76076eb7c40f4ff65ea908d
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-29 13:05:20 +0530

    Native Query Implemented

--- src/main/java/com/springjpa/Repo/StudentRepo.java
+++ src/main/java/com/springjpa/Repo/StudentRepo.java
@@ -2,6 +2,7 @@ package com.springjpa.Repo;
 
 import com.springjpa.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,12 @@ import java.util.List;
 public interface StudentRepo  extends JpaRepository<Student,Long> {
     public List<Student> findByFristName(String fristName);
     public  List<Student> findByFristNameContaining(String str);
+//Native Query
+    @Query(
+            value = "select * from jpa_student j where  j.email=?1",
+            nativeQuery = true
+    )
+    Student getStudentByEmailId(String email);
+    //JPL Query
+
 }
