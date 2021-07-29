c35a28af4b3cafa2b3f1a9f1c612a97b29e60719
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-07-29 11:40:45 +0530

    Student Entity created Student Table Logic

--- src/main/java/com/springjpa/Entity/Student.java
+++ src/main/java/com/springjpa/Entity/Student.java
@@ -1,4 +1,27 @@
 package com.springjpa.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
+    @Id
+//    @GeneratedValue()
+    private  Long StudentId;
+    private  String fristName;
+    private  String lastName;
+    private  String emailId;
+    private  String gurdianName;
+    private  String GurdianEmail;
+    private  String gurdianMobail;
 }
